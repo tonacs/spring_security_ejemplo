91ba56f25b51c6433116fe31fba730c2b57a7b1d
Tona <chilakiller@chilakiller.com>
2023-08-09 19:18:59 -0600

    Implementacion de jdbc user detail manager

--- src/main/java/com/tona/app_security/security/SecurityConfig.java
+++ src/main/java/com/tona/app_security/security/SecurityConfig.java
@@ -4,7 +4,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -17,5 +26,29 @@ public class SecurityConfig {
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+/*@Bean
+    InMemoryUserDetailsManager inMemoryUserDetailsManager(){
+        var admin= User.withUsername("admin")
+                .password("to_be_encoded")
+                .authorities("ADMIN")
+                .build();
+
+        var user= User.withUsername("user")
+                .password("to_be_encoded")
+                .authorities("USER")
+                .build();
+
+    return new InMemoryUserDetailsManager(admin, user);
+    }
+*/
+    @Bean
+    UserDetailsService userDetailsService(DataSource dataSource){
+    return new JdbcUserDetailsManager(dataSource);
+    }
+    @Bean
+    PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
